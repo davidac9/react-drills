022a2277493aae774c35f153b581c2d66145a384
david collett <davida.collett95@gmail.com>
2019-07-09 16:24:53 -0600

    done

---
+++ app-4/src/components/LoginButton.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class LoginButton extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <button onClick={() => {this.props.al()} }>Login</button>
+        )
+    }
+}
